184857ca8a2822d008f29358fc2973b9d06e3c01
Rafael Nascimento <rafael.93rj@gmail.com>
2022-12-26 15:30:42 -0300

    clean js css

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const elementoInfo = document.querySelector("#info")
-const buttonSaibaMais = document.querySelector("#saibaMais")
 
-function saibaMais () {
-    document.getElementById("info").innerHTML = "<p>Olá, me chamo Rafael, tenho 29 anos e estou em busca de uma oportunidade de estágio.<br> No momento estou cursando Análise e Desenvolvimento de Sistemas pela Unisuam e participando do Programa Desenvolve do Grupo Boticário em parceiria com a Alura.<br> Você pode conferir um pouco mais do meu currículo e projetos e um dos links acima.</p>"
+let buttonSaibaMais = document.querySelector("#saibaMais")
+let divInfo = document.querySelector("#info");
 
-    elementoInfo.style.opacity = 1;
+divInfo.style.display = "none";
+
+function saibaMais() {
+    if (divInfo.style.display === "none") {
+        divInfo.style.display = "block";
+    } 
+    
+    divInfo.style.opacity = 1;
     setTimeout (function () {
-        elementoInfo.style.opacity = 0
+        divInfo.style.opacity = 0
         buttonSaibaMais.removeAttribute("disable")
         }, 25000)
-}
-
-
-
+    }
